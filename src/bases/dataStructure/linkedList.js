2fc63a9f56c5911448f175e523df5903322ea073
Gabriel <gabrielnascimento.lima@portoseguro.com.br>
2024-03-20 21:31:11 -0300

    feat: linkedList.pop()

--- src/bases/dataStructure/linkedList.js
+++ src/bases/dataStructure/linkedList.js
@@ -7,14 +7,14 @@ class Node {
 }
 
 class LinkedList {
-  
+
   constructor(value) {
-    const node = new Node(value)
-    this.head = node
+    const newNode = new Node(value)
+    this.head = newNode
     this.tail = this.head
     this.length = 1
   }
-  
+
   push(value) {
     const newNode = new Node(value)
     if (this.head === null) {
@@ -22,25 +22,63 @@ class LinkedList {
       this.head = newNode
       this.tail = newNode
     }
-    else {
-      console.log('else-> tail:', this.tail)
+    else { //this.tail points to the same object as head.
       this.tail.next = newNode
-      console.log('else-> tail:', this.tail)
       this.tail = newNode
-      console.log('else-> tail:', this.tail)
     }
     this.length++
     return this
   }
+
+  pop() {
+    if (!this.head) return undefined
+    if (this.head.length === 1) return this.head = this.tail = null
+
+    let temp = this.head
+    let pre = this.head
+
+    while(temp.next) {
+      pre = temp
+      temp = temp.next
+    }
+    //In lists with one element tail points to the same object as head
+    //this.tail = {...pre, next: null} //Not the sema object pointed by last element
+    this.tail = pre
+    this.tail.next = null
+    this.length--
+    return temp
+  }
+
+
+  // Auxiliary
+  #printList(message) {
+    console.log(message, myLinkedList)
+  }
+
+  makeEmpty() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
 }
 
 
 //const node1 = new Node(10)
 //console.log('node1:', node1)
 
-const myLinkedList = new LinkedList(10)
-console.log(myLinkedList)
-myLinkedList.push(20)
+const myLinkedList = new LinkedList(1)
+myLinkedList.push(2)
+myLinkedList.push(3)
+myLinkedList.push(4)
+
+console.log(myLinkedList.pop())
+console.log(myLinkedList.pop())
+console.log(myLinkedList.pop())
+console.log(myLinkedList.pop())
+
+
+
 
 
 
